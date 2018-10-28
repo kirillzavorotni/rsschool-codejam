0072da459eb43ec6561d8bb946b1ea17b10dec40
Kiryl Zavarotny <kirillzavorotni@gmail.com>
2018-10-28 16:52:02 +0300

    feat(make.js) add make function

---
+++ make.js
@@ -0,0 +1,19 @@
+function make(...args){
+    
+    let storage = [];
+    storage = storage.concat(args);
+    
+    function wrap(...args){
+        let toString = Object.prototype.toString;
+        
+        if( toString.call(args[0]).indexOf('Number') !== -1 ){
+            args.forEach(elem => storage.push(elem));
+        } else {
+            return storage.reduce(args[0]);
+        }
+        
+        return wrap;
+    }
+    
+    return wrap;
+}
